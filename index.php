4816d91143a43e271c0f8b3ec4f0c72f569e0524
Jim Birch <git@jimbir.ch>
2019-07-02 09:35:01 -0400

    Init commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>The Pearl of Yarmouth</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="The Pearl of Yarmouth | Coming Soon.">
+    <link rel="canonical" href="https://www.pearlofyarmouth.com">
+    <link href="./files/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="./files/cover.css" rel="stylesheet">
+  </head>
+  <body class="text-center">
+    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+      <header class="masthead mb-auto">
+        <div class="inner">
+          <div class="masthead-brand">The Pearl of Yarmouth</div>
+          <nav class="nav nav-masthead justify-content-center">
+            <a class="nav-link active" href="/">Home</a>
+            <a class="nav-link" href="https://instagram.com/pearlofyarmouth">Instagram</a>
+            <a class="nav-link" href="https://twitter.com/pearlofyarmouth">Twitter</a>
+          </nav>
+        </div>
+      </header>
+      <main role="main" class="inner cover">
+        <h1 class="cover-heading">The Pearl of Yarmouth</h1>
+        <p class="lead">Coming soon!</p>
+        <p class="lead">
+          <a href="https://instagram.com/pearlofyarmouth" class="btn btn-lg btn-secondary">Learn more</a>
+        </p>
+      </main>
+      <footer class="mastfoot mt-auto">
+        <div class="inner">
+          <p>&copy; 2019 <a href="/">pearlofyarmouth.com</a>. All Rights Reserved.</p>
+        </div>
+      </footer>
+    </div>
+  </body>
+</html>
